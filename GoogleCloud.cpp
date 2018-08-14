a4c017375c1ebba1bc0730f928aa9f42cf4449c4
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-14 10:06:14 +0900

    Add files via upload

---
+++ GoogleCloud.cpp
@@ -0,0 +1,16 @@
+/*
+ * GoogleCloud.cpp
+ *
+ *  Created on: Aug 14, 2018
+ *      Author: tungdang
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+	cout << "Let's to study " << endl;
+	return 0;
+}
+
