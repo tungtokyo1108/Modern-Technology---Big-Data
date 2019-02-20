8a0b66a437d12faa1ff07a6b4df466a3430ef36b
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-02-20 16:09:38 +0900

    Update JobConfiguration.java

--- Google - BigQuery/JobConfiguration.java
+++ Google - BigQuery/JobConfiguration.java
@@ -9,8 +9,15 @@
 package com.google.cloud.bigquery.mirror;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.cloud.bigquery.CopyJobConfiguration;
+import com.google.cloud.bigquery.ExtractJobConfiguration;
+import com.google.cloud.bigquery.LoadConfiguration;
+import com.google.cloud.bigquery.QueryJobConfiguration;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.MoreObjects.ToStringHelper;
+import com.google.rpc.RetryInfo;
+
 import java.io.Serializable;
 import java.util.Objects;
 
@@ -49,5 +56,55 @@ public abstract class JobConfiguration implements Serializable
         public abstract T build();
     }
 
-    
+    JobConfiguration(Builder builder)
+    {
+        this.type = builder.type;
+    }
+
+    public Type getType()
+    {
+        return type;
+    }
+
+    public abstract Builder toBuilder();
+
+    ToStringHelper toStringHelper() 
+    {
+        return MoreObjects.toStringHelper(this).add("Type", type);
+    }
+
+    @Override
+    public String toString() 
+    {
+        return toStringHelper().toString();
+    }
+
+    final int baseHashCode()
+    {
+        return Objects.hash(type);
+    }
+
+    final boolean baseEquals(JobConfiguration jobConfiguration)
+    {
+        return Objects.equals(toPb(), jobConfiguration.toPb());
+    }
+
+    abstract JobConfiguration setProjectId(String projectId);
+    abstract com.google.api.services.bigquery.model.JobConfiguration toPb();
+    @SuppressWarnings("unchecked")
+    static<T extends JobConfiguration> T fromPb(
+        com.google.api.services.bigquery.model.JobConfiguration configurationPb) {
+        if (configurationPb.getCopy() != null)
+        {
+            return (T) CopyJobConfiguration.fromPb(configurationPb);
+        } else if (configurationPb.getExtract() != null) {
+            return (T) ExtractJobConfiguration.fromPb(configurationPb);
+        } else if (configurationPb.getLoad() != null) {
+            return (T) LoadConfiguration.fromPb(configurationPb);
+        } else if (configurationPb.getQuery() != null) {
+            return (T) QueryJobConfiguration.fromPb(configurationPb);
+        } else {
+            throw new IllegalArgumentException("Job Configuration is not supported");
+        }
+    }
 }
