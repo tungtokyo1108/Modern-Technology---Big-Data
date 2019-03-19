b52cdac60642ff9732cd0c59f8e1dbe159435ded
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-03-19 19:56:12 +0900

    Update ADLFileInputStream.java

--- Microsoft-DataLake/src/ADLFileInputStream.java
+++ Microsoft-DataLake/src/ADLFileInputStream.java
@@ -71,4 +71,162 @@ public class ADLFileInputStream extends InputStream
         if (i < 0) return i;
         else return (b[0] & 0xFF);
     }
+
+    @Override
+    public int read(byte[] b) throws IOException 
+    {
+        if (b == null)
+        {
+            throw new IllegalArgumentException("null byte array passed in to read() method");
+        }
+        return read(b, 0, b.length);
+    }
+
+    @Override 
+    public int read(byte[] b, int off, int len) throws IOException
+    {
+        if (streamClosed) throw new IOException("attempting to read from a closed stream");
+        if (b == null)
+        {
+            throw new IllegalArgumentException("null byte array passed in to read() method");
+        }
+        
+        if (off < 0 || len < 0 || len > b.length - off) 
+        {
+            throw new IndexOutOfBoundsException();
+        }
+        if (log.isTraceEnabled())
+        {
+            log.trace("ADLFileInputStream.read(b,off,{}) at offset {} using client {} from file {}", len, getPos(), client.getClientId(), filename);
+        }
+
+        if (len == 0)
+        {
+            return 0;
+        }
+
+        if (bCursor == limit)
+        {
+            if (readFromService() < 0) return -1;
+        }
+
+        int bytesRemaining = limit - bCursor;
+        int bytesToRead = Math.min(len, bytesRemaining);
+        System.arraycopy(buffer, bCursor, b, off, bytesToRead);
+        bCursor += bytesToRead;
+        return bytesToRead;
+    }
+
+    /**
+     * Read from service try to read bytes from service.
+     * Return how many bytes are actually reads, could be less than blocksize
+     */
+    protected long readFromService() throws IOException
+    {
+        if (bCursor < limit) return 0;
+        if (fCursor >= directoryEntry.length) return -1;
+
+        if (directoryEntry.length <= blocksize)
+        {
+            return slurpFullFile();
+        }
+
+        bCursor = 0;
+        limit = 0;
+        if (buffer == null) buffer = new byte[blocksize];
+
+        int bytesRead = readInternal(fCursor, buffer, 0, blocksize, false);
+        limit += bytesRead;
+        fCursor += bytesRead;
+        return bytesRead;
+    }
+
+    /**
+     * Reads the whole file into buffer
+     */
+    protected long slurpFullFile() throws IOException
+    {
+        if (log.isTraceEnabled())
+        {
+            log.trace("ADLFileInputStream.slurpFullFile() - using client {} from file {}. At offset {}", client.getClientId(), filename, getPos());
+        }
+
+        if (buffer == null)
+        {
+            blocksize = (int) directoryEntry.length;
+            buffer = new byte[blocksize];
+        }
+        
+        // Preserve current file offset
+        bCursor = (int) getPos();
+        limit = 0;
+        // Read from begining
+        fCursor = 0;
+        int loopCount = 0;
+
+        // If one Open request doesnt get full file, then read again at fCursor
+        while (fCursor < directoryEntry.length)
+        {
+            int bytesRead = readInternal(fCursor, buffer, limit, blocksize - limit, true);
+            limit += bytesRead;
+            fCursor += bytesRead;
+
+            // Defensive against infinite loops 
+            loopCount++;
+            if (loopCount >= 10)
+            {
+                throw new IOException("Too many attempts to read whole file" + filename);
+            }
+        }
+        return fCursor;
+    }
+
+    public int read(long position, byte[] b, int offset, int length) throws IOException
+    {
+        if (streamClosed) throw new IOException("attempting to read from a closed stream");
+        if (log.isTraceEnabled())
+        {
+            log.trace("ADLFileInputStream positioned read() - at offset {} using client {} from file {}", position, client.getClientId(), filename);
+        }
+        return readInternal(position, b, offset, length, true);
+    }
+
+    private int readInternal(long position, byte[] b, int offset, int length, boolean bypassReadAhead) throws IOException
+    {
+        boolean readAheadEnabled = true;
+        if (readAheadEnabled && !bypassReadAhead && client.disableReadAheads)
+        {
+            // try reading from read-ahead
+            if (offset != 0) throw new IllegalArgumentException("readahead buffers cannot have non-zero buffer offsets");
+            int receivedBytes;
+
+            // queue read-aheads
+            int numReadAheads = this.readAheadQueueDepth;
+            long nextSize;
+            long nextOffset = position;
+            while (numReadAheads > 0 && nextOffset < directoryEntry.length)
+            {
+                nextSize = Math.min((long)blocksize, directoryEntry.length - nextOffset);
+                if (log.isTraceEnabled())
+                {
+                    log.trace("Queueing readAhead for file" + filename + "offset" + nextOffset + "thread" + Thread.currentThread().getName());
+                }
+                ReadBufferManager.getBufferManager().queueReadAhead(this, nextOffset, (int) nextSize);
+                nextOffset = nextOffset + nextSize;
+                numReadAheads--;   
+            }
+
+            // try reading from buffers first 
+            receivedBytes = ReadBufferManager.getBufferManager().getBlock(this, position, length, b);
+            if (receivedBytes > 0) return receivedBytes;
+            
+            // got nothing from read-ahead, do our own read now
+            receivedBytes = readRemote(position, b, offset, length, false);
+            return receivedBytes;
+        }
+        else 
+        {
+            return readRemote(position, b, offset, length, false);
+        }
+    }
 }
