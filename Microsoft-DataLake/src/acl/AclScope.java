119fc615d377ba1c883d026bc3b498c382ad1208
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-05-29 11:27:48 +0900

    Create AclScope.java

---
+++ Microsoft-DataLake/src/acl/AclScope.java
@@ -0,0 +1,14 @@
+/**
+ * Microsoft - Big Data Technology
+ * https://github.com/Azure/azure-data-lake-store-java/blob/master/src/main/java/com/microsoft/azure/datalake/store/acl/AclScope.java
+ *
+ *  Created on: May 28, 2019
+ *  Data Scientist: Tung Dang
+ */
+
+package com.microsoft.azure.datalake.store.mirror.acl;
+
+public enum AclScope {
+    ACCESS,
+    DEFAULT
+}
