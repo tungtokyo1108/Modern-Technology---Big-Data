cc464c2fa4efbec8f1f43e4cef40587631e728a4
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-05-29 11:27:16 +0900

    Create AclType.java

---
+++ Microsoft-DataLake/src/acl/AclType.java
@@ -0,0 +1,16 @@
+/**
+ * Microsoft - Big Data Technology
+ * https://github.com/Azure/azure-data-lake-store-java/blob/master/src/main/java/com/microsoft/azure/datalake/store/acl/AclType.java
+ *
+ *  Created on: May 28, 2019
+ *  Data Scientist: Tung Dang
+ */
+
+package com.microsoft.azure.datalake.store.mirror.acl;
+
+public enum AclType {
+    USER,
+    GROUP,
+    OTHER,
+    MASK
+}
